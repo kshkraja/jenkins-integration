1e9691c116f08163a4c98da9c8527d7b570a4c25
kshkraja <50064073+kshkraja@users.noreply.github.com>
2021-10-18 00:21:58 +0530

    Update provider.tf

--- aws-instance-first-script/provider.tf
+++ aws-instance-first-script/provider.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "${var.region}"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
